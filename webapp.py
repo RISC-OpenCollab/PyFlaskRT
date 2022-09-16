e785a5e2181385a5bd8cc8bd373875a6d9e03478
jamesfgreene <jamesfgreene@gmail.com>
2022-09-16 14:53:46 -0700

    Update webapp.py
    Cleaned up the sample code a bit.    Correctly assigned "http_response_code" and "response" for  the runtime scanner AFTER API request

--- webapp.py
+++ webapp.py
@@ -9,12 +9,18 @@ def index():
     #example simple API call
     query = {'lat':'45', 'lon':'180'}
     headers = {"Content-Type": "application/json; charset=utf-8"}
+    
+    # Runtime scanner BEFORE API request
     url = 'https://portaleast.riscosity.com/api/v1/rtscanner/organizations/94073c7a-0533-11ec-b635-3dca3bca46fd/products/447e3cb3-35d5-11ed-b99b-028b315293dd/repos/58c234e0-35d5-11ed-b99b-028b315293dd/ses/5b34287c-35d5-11ed-b99b-028b315293dd/log_service_endpoint_before'
     myobj = {"url": "http://api.open-notify.org/iss-pass.json", "headers": headers, "method":"GET", "request_body":query}
     x = requests.post(url, json = myobj)
+
+    # Target API request
     response = requests.get('http://api.open-notify.org/iss-pass.json', params=query)
-    url = 'https://portaleast.riscosity.com/api/v1/rtscanner/organizations/94073c7a-0533-11ec-b635-3dca3bca46fd/products/447e3cb3-35d5-11ed-b99b-028b315293dd/repos/58c234e0-35d5-11ed-b99b-028b315293dd/ses/5b34287c-35d5-11ed-b99b-028b315293dd/log_service_endpoint_before'
-    myobj = {"url": "http://api.open-notify.org/iss-pass.json", "http_response_code": x.status_code, "response":x.text}
+    
+    # Runtime scanner AFTER API request
+    url = 'https://portaleast.riscosity.com/api/v1/rtscanner/organizations/94073c7a-0533-11ec-b635-3dca3bca46fd/products/447e3cb3-35d5-11ed-b99b-028b315293dd/repos/58c234e0-35d5-11ed-b99b-028b315293dd/ses/5b34287c-35d5-11ed-b99b-028b315293dd/log_service_endpoint_after'
+    myobj = {"url": "http://api.open-notify.org/iss-pass.json", "http_response_code": response.status_code, "response":response.text}
     x = requests.post(url, json = myobj)
     
     #example variable name, assigned to value in code
